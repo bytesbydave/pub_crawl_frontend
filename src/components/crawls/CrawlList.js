10fc0c6bbef7e8f0ba984d020a1a312eb7b4870b
Dave <dave.lin21@gmail.com>
2019-02-24 23:45:59 -0500

    initial setup for crawl CRUD

---
+++ src/components/crawls/CrawlList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CrawlList extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2>List of Pubcrawls</h2>
+      </div>
+    )
+  }
+}
+
+export default CrawlList;
