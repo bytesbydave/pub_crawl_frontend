e1562696c5be57245cc374de2651a146ecaefee7
Dave <dave.lin21@gmail.com>
2019-02-26 00:12:09 -0500

    add crawl reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 import streamReducer from './streamReducer';
-import crawlReducer from './crawlReducer'
+import crawlReducer from './crawlReducer';
+import locationReducer from './locationReducer';
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
   streams: streamReducer,
-  crawls: crawlReducer
+  crawls: crawlReducer,
+  locations: locationReducer
 });
