9e51f6f566eef8bedc33312c5dd182860b493e6c
Dave <dave.lin21@gmail.com>
2019-02-26 00:09:40 -0500

    create location reducer

---
+++ src/reducers/locationReducer.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import {
+  CREATE_LOCATION,
+  FETCH_LOCATIONS,
+  FETCH_LOCATION,
+  DELETE_LOCATION
+} from '../actions/types';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_LOCATIONS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
+    case FETCH_LOCATION:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_LOCATION:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_LOCATION:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
