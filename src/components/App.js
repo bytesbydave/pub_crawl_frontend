a4e9b1fbcce1ffd140705df5162db8d6df139bca
Dave <dave.lin21@gmail.com>
2019-02-28 05:45:02 -0500

    add routes for homepage and footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
+import Home from './application/Home';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -12,22 +13,27 @@ import CrawlDelete from './crawls/CrawlDelete';
 import CrawlShow from './crawls/CrawlShow';
 import CrawlAddLocations from './crawls/CrawlAddLocations';
 import LocationShow from './locations/LocationShow';
-import Header from './Header';
+import Header from './application/Header';
+import Footer from '../components/application/Footer';
 import history from '../history';
 
 const App = () => {
   return (
-    <div className="ui container">
+    <div>
       <Router history={history}>
         <div>
           <Header />
           <Switch>
-            <Route path="/" exact component={CrawlList} />
+            <Route path="/" exact component={Home} />
+            <Route path="/crawls" exact component={CrawlList} />
             <Route path="/crawls/new" component={CrawlCreate} />
             <Route path="/crawls/edit/:id" component={CrawlEdit} />
             <Route path="/crawls/delete/:id" component={CrawlDelete} />
             <Route path="/crawls/:id" exact component={CrawlShow} />
-            <Route path="/crawls/:id/add_locations" component={CrawlAddLocations} />
+            <Route
+              path="/crawls/:id/add_locations"
+              component={CrawlAddLocations}
+            />
             <Route path="/locations/:id" component={LocationShow} />
 
             <Route path="/streams" exact component={StreamList} />
@@ -36,6 +42,7 @@ const App = () => {
             <Route path="/streams/delete/:id" component={StreamDelete} />
             <Route path="/streams/:id" component={StreamShow} />
           </Switch>
+          <Footer />
         </div>
       </Router>
     </div>
