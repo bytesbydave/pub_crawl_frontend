921f17d81a6cac1df0412f63a30975d0749dc05a
Dave <dave.lin21@gmail.com>
2019-02-28 05:43:25 -0500

    redesign display of pub crawls

--- src/components/crawls/CrawlShow.js
+++ src/components/crawls/CrawlShow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { fetchCrawl, fetchLocations } from '../../actions';
+import './Crawl.css'
 
 class CrawlShow extends React.Component {
   componentDidMount() {
@@ -12,12 +12,34 @@ class CrawlShow extends React.Component {
   renderList() {
     return this.props.locations.map(location => {
       return (
-        <div className="location" key={location.id}>
+        <div className="item" key={location.id}>
+          <div className="image">
+            <img src={location.image_url} alt="bar" />
+          </div>
           <div className="content">
-            <Link to={`/locations/${location.id}`} className="header">
+            <a
+              className="header"
+              href={location.website}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               {location.name}
-            </Link>
-            <div className="description">{location.description}</div>
+            </a>
+            <div className="meta">
+              <span className="cinema">{location.address}</span>
+            </div>
+            <div className="meta">
+              <span className="cinema">{location.city}</span>
+            </div>
+            <div className="description">
+              <p>{location.description}</p>
+            </div>
+            <div className="extra">
+              <div className="ui label">Rating: {location.rating} Stars</div>
+              <div className="ui label">
+                <i className="globe icon" /> {location.category}
+              </div>
+            </div>
           </div>
         </div>
       );
@@ -29,10 +51,9 @@ class CrawlShow extends React.Component {
       return <div>Loading...</div>;
     }
     return (
-      <div>
-        <h2>{this.props.crawl.name}</h2>
-        <p>{this.props.crawl.description}</p>
-        {this.renderList()}
+      <div className="ui container screen-container">
+        <h1>Pub Crawl Itinerary for {this.props.crawl.name}</h1>
+        <div className="ui divided items">{this.renderList()}</div>
       </div>
     );
   }
