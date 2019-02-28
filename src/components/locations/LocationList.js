0d620f84a0c441ecb1a2a532fb6b66c3930a39ba
Dave <dave.lin21@gmail.com>
2019-02-28 05:42:15 -0500

    add props to LocationCreate

--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -8,8 +8,19 @@ class LocationList extends React.Component {
     const crawlId = this.props.crawl;
     return this.props.locations.map(location => {
       return (
-        <div key={location.id}>
-          <LocationCreate name={location.name} crawlId={crawlId} />
+        <div key={location.id} className="ui divided items">
+          <LocationCreate 
+            name={location.name} 
+            crawlId={crawlId}
+            imageUrl={location.image_url} 
+            website={location.url} 
+            phone={location.display_phone}
+            rating={location.rating}
+            yelpId={location.id}
+            category={location.categories}
+            city={location.location.city}
+            address={location.location.address1}
+           />
         </div>
       );
     });
