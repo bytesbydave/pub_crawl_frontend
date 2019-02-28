36884ab85e3aec47248e60d9167be6d569f84986
Dave <dave.lin21@gmail.com>
2019-02-28 05:41:43 -0500

    create tests for actions

---
+++ src/actions/__tests__/index.test.js
@@ -0,0 +1,41 @@
+import {
+  signIn,
+  signOut,
+  createLocation,
+  fetchLocation,
+  fetchLocations,
+  deleteLocation,
+  editLocation,
+  createCrawls,
+  fetchCrawls,
+  fetchCrawlss,
+  deleteCrawls,
+  editCrawls
+} from '../../actions';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_CRAWL,
+  FETCH_CRAWLS,
+  FETCH_CRAWL,
+  DELETE_CRAWL,
+  EDIT_CRAWL,
+  CREATE_LOCATION,
+  FETCH_LOCATIONS,
+  FETCH_LOCATION,
+  DELETE_LOCATION
+} from '../../actions/types';
+
+describe('createLocation', () => {
+  // it('has the correct type', () => {
+  //   const action = createLocation();
+
+  //   expect(action.type).toEqual(CREATE_LOCATION);
+  // });
+
+  // it('has the correct payload', () => {
+  //   const action = createLocation('1', 'Rudys bar');
+
+  //   expect(action.payload).toEqual('New Comment');
+  // });
+});
