f85e33320d1ad86f39a4d95603aa7d53993a1c8f
Dave <dave.lin21@gmail.com>
2019-02-28 05:46:08 -0500

    design layout of render bar location

--- src/components/crawls/CrawlAddLocations.js
+++ src/components/crawls/CrawlAddLocations.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchCrawl, fetchLocations } from '../../actions';
-import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import LocationList from '../locations/LocationList';
 import yelp from '../../apis/yelp';
@@ -21,18 +20,33 @@ class CrawlAddLocation extends React.Component {
   renderList() {
     return this.props.locations.map(location => {
       return (
-        <div className="location" key={location.id}>
+        <div className="item" key={location.id}>
+          <div className="image">
+            <img src={location.image_url} alt="bar" />
+          </div>
           <div className="content">
-            <Link to={`/locations/${location.id}`} className="header">
-              {location.name}
-            </Link>
-            <div className="description">{location.description}</div>
+            <a className="header" href={location.website} target="_blank" rel="noopener noreferrer">{location.name}</a>
+            <div className="meta">
+              <span className="cinema">{location.address}</span>
+            </div>
+            <div className="meta">
+              <span className="cinema">{location.city}</span>
+            </div>
+            <div className="description">
+              <p>{location.description}</p>
+            </div>
+            <div className="extra">
+              <div className="ui label">Rating: {location.rating} Stars</div>
+              <div className="ui label">
+                <i className="globe icon" /> {location.category}
+              </div>
+            </div>
           </div>
         </div>
       );
     });
   }
-  
+
   onTermSubmit = async term => {
     const response = await yelp.get('/businesses/search', {
       params: {
@@ -42,7 +56,7 @@ class CrawlAddLocation extends React.Component {
         categories: 'bars'
       }
     });
-    this.setState({ locations: response.data.businesses});
+    this.setState({ locations: response.data.businesses });
   };
 
   render() {
@@ -50,10 +64,14 @@ class CrawlAddLocation extends React.Component {
       return <div>Loading...</div>;
     }
     return (
-      <div>
+      <div className="ui container screen-container">
+        <h1>Add some bars to {this.props.crawl.name}</h1>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        {this.renderList()}
-        <LocationList locations={this.state.locations} crawl={this.props.crawl.id} />
+        <LocationList
+          locations={this.state.locations}
+          crawl={this.props.crawl.id}
+        />
+        <div className="ui divided items">{this.renderList()}</div>
       </div>
     );
   }
@@ -63,7 +81,10 @@ const mapStateToProps = (state, ownProps) => {
   return {
     crawl: state.crawls[ownProps.match.params.id],
     locations: Object.values(state.locations)
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, { fetchCrawl, fetchLocations })(CrawlAddLocation);
+export default connect(
+  mapStateToProps,
+  { fetchCrawl, fetchLocations }
+)(CrawlAddLocation);
