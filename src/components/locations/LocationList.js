4fa2c94d4117f07ef010335441104f70a86e0ef9
Dave <dave.lin21@gmail.com>
2019-03-04 02:14:12 -0500

    make sure search list does not contain bars on added list

--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -4,25 +4,36 @@ import { createLocation } from '../../actions';
 import LocationCreate from './LocationCreate';
 
 class LocationList extends React.Component {
+  findBarId(locations) {
+    const bars = [];
+    for (let bar of locations) {
+      bars.push(bar.yelp_id);
+    }
+    return bars;
+  }
+
   render() {
     const crawlId = this.props.crawl;
+    const addedBars = this.findBarId(this.props.addedLocations);
     return this.props.locations.map(location => {
-      return (
-        <div key={location.id} className="ui divided items">
-          <LocationCreate 
-            name={location.name} 
-            crawlId={crawlId}
-            imageUrl={location.image_url} 
-            website={location.url} 
-            phone={location.display_phone}
-            rating={location.rating}
-            yelpId={location.id}
-            category={location.categories}
-            city={location.location.city}
-            address={location.location.address1}
-           />
-        </div>
-      );
+      if (!addedBars.includes(location.id)) {
+        return (
+          <div key={location.id} className="ui divided items">
+            <LocationCreate
+              name={location.name}
+              crawlId={crawlId}
+              imageUrl={location.image_url}
+              website={location.url}
+              phone={location.display_phone}
+              rating={location.rating}
+              yelpId={location.id}
+              category={location.categories}
+              city={location.location.city}
+              address={location.location.address1}
+            />
+          </div>
+        );
+      }
     });
   }
 }
