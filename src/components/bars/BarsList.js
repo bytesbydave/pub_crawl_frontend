85fc344a75427a5e669b6e3d2a6b7d38755ee2ac
Dave <dave.lin21@gmail.com>
2019-04-29 00:36:54 -0400

    create bar list component

---
+++ src/components/bars/BarsList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import BarItem from './BarItem';
+import { connect } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css';
+
+class BarList extends React.Component {
+  renderBarList() {
+    if (this.props.error) {
+      return <div>Error</div>;
+    }
+    if (this.props.isPending) {
+      return <div>Loading</div>;
+    } else {
+      return this.props.bars.map(bar => {
+        return <BarItem bar={bar} key={bar.id} picture={bar.imageUrl} />;
+      });
+    }
+  }
+  render() {
+    return <div className="ui link cards">{this.renderBarList()}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    bars: state.requestBars.bars,
+    isPending: state.requestBars.isPending,
+    error: state.requestBars.error
+  };
+};
+
+export default connect(mapStateToProps)(BarList);
