9716e0383b60564221888eded820cdd6ccc37c82
Dave <dave.lin21@gmail.com>
2019-04-29 00:40:01 -0400

    add search component to crawl show

--- src/components/crawls/CrawlShow.js
+++ src/components/crawls/CrawlShow.js
@@ -1,49 +1,58 @@
 import React from 'react';
+import BarsDisplay from '../bars/BarsDisplay';
 import { connect } from 'react-redux';
 import { fetchCrawl, fetchLocations } from '../../actions';
-import './Crawl.css'
+import './Crawl.css';
 
 class CrawlShow extends React.Component {
   componentDidMount() {
     this.props.fetchCrawl(this.props.match.params.id);
-    this.props.fetchLocations(this.props.match.params.id);
+    // this.props.fetchLocations(this.props.match.params.id);
   }
 
   renderList() {
-    return this.props.locations.map(location => {
-      return (
-        <div className="item" key={location.id}>
-          <div className="image">
-            <img src={location.image_url} alt="bar" />
-          </div>
-          <div className="content">
-            <a
-              className="header"
-              href={location.website}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {location.name}
-            </a>
-            <div className="meta">
-              <span className="cinema">{location.address}</span>
-            </div>
-            <div className="meta">
-              <span className="cinema">{location.city}</span>
-            </div>
-            <div className="description">
-              <p>{location.description}</p>
-            </div>
-            <div className="extra">
-              <div className="ui label">Rating: {location.rating} Stars</div>
-              <div className="ui label">
-                <i className="globe icon" /> {location.category}
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    });
+    // return this.props.locations.map(location => {
+    //   return (
+    //     <div className="item" key={location.id}>
+    //       <div className="image">
+    //         <img src={location.image_url} alt="bar" />
+    //       </div>
+    //       <div className="content">
+    //         <a
+    //           className="header"
+    //           href={location.website}
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //         >
+    //           {location.name}
+    //         </a>
+    //         <div className="meta">
+    //           <span className="cinema">{location.address}</span>
+    //         </div>
+    //         <div className="meta">
+    //           <span className="cinema">{location.city}</span>
+    //         </div>
+    //         <div className="description">
+    //           <p>{location.description}</p>
+    //         </div>
+    //         <div className="extra">
+    //           <div className="ui label">Rating: {location.rating} Stars</div>
+    //           <div className="ui label">
+    //             <i className="globe icon" /> {location.category}
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   );
+    // });
+  }
+
+  renderBarsSearch() {
+    return (
+      <div>
+        <BarsDisplay />
+      </div>
+    );
   }
 
   render() {
@@ -54,6 +63,7 @@ class CrawlShow extends React.Component {
       <div className="ui container screen-container">
         <h1>Pub Crawl Itinerary for {this.props.crawl.name}</h1>
         <div className="ui divided items">{this.renderList()}</div>
+        <div>{this.renderBarsSearch()}</div>
       </div>
     );
   }
@@ -61,8 +71,8 @@ class CrawlShow extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    crawl: state.crawls[ownProps.match.params.id],
-    locations: Object.values(state.locations)
+    crawl: state.crawls[ownProps.match.params.id]
+    // locations: Object.values(state.locations)
   };
 };
 
