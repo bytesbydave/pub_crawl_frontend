95bfabe8bdde8dd201e03acfad3ea848360eda59
Dave <dave.lin21@gmail.com>
2019-04-30 16:12:54 -0400

    remove uncessary imports

--- src/components/bars/BarItem.js
+++ src/components/bars/BarItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
 import { createLocation } from '../../actions';
 import 'semantic-ui-css/semantic.min.css';
 import './Bar.css';
