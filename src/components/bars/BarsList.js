0c52c2311c5fb491438bb80c43c67b86e93006b7
Dave <dave.lin21@gmail.com>
2019-04-30 15:24:59 -0400

    filter bars that have been selected already

--- src/components/bars/BarsList.js
+++ src/components/bars/BarsList.js
@@ -4,23 +4,37 @@ import { connect } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 
 class BarList extends React.Component {
-  renderBarList(crawl) {
-    const crawlId = crawl.id
+  renderBarList(crawl, locations) {
+    const crawlId = crawl.id;
+    const locationIds = locations.map(location => location.yelp_id);
+    const bars = [];
+    for (let bar of this.props.bars) {
+      if (!locationIds.includes(bar.id)) {
+        bars.push(bar);
+      }
+    }
     if (this.props.error) {
       return <div>Error</div>;
     }
     if (this.props.isPending) {
       return <div>Loading</div>;
     } else {
-      return this.props.bars.map(bar => {
-        return <BarItem  bar={bar} key={bar.id} picture={bar.imageUrl} crawl={crawlId} />;
+      return bars.map(bar => {
+        return (
+          <BarItem
+            bar={bar}
+            key={bar.id}
+            picture={bar.imageUrl}
+            crawl={crawlId}
+          />
+        );
       });
     }
   }
   render() {
     return (
       <div className="ui link cards">
-        {this.renderBarList(this.props.crawl)}
+        {this.renderBarList(this.props.crawl, this.props.locations)}
       </div>
     );
   }
