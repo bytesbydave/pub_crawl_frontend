df4498ae97d2f8c22ad056a4fcb208886e63cf2f
Dave <dave.lin21@gmail.com>
2019-04-30 03:23:53 -0400

    designs for bar item card

--- src/components/bars/BarItem.js
+++ src/components/bars/BarItem.js
@@ -1,26 +1,57 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { createLocation } from '../../actions';
 import 'semantic-ui-css/semantic.min.css';
+import './Bar.css';
 
-const BarItem = ({ bar }) => {
-  console.log(bar)
-  return (
-    <div className="card">
-      <div className="image">
-        <img src={bar.image_url} alt="bar-pic" />
-      </div>
-      <div className="content">
-        <div className="header">{bar.name}</div>
-        <div className="meta">
-          <a>Category</a>
+class BarItem extends React.Component {
+  render() {
+    const { bar } = this.props;
+    const renderCategories = bar.categories.map(category => {
+      return <span key={category.title}>| {category.title}</span>;
+    });
+    const displayAddress = bar.location.display_address.map(address => {
+      return <span key={address}> {address} </span>;
+    });
+
+    return (
+      <div className="card">
+        <div className="image bar-img">
+          <img src={bar.image_url} alt="bar-pic" />
         </div>
-        <div className="description">Description</div>
+        <div className="content">
+          <div className="header">{bar.name}</div>
+          <div className="meta">{renderCategories}</div>
+          <div className="description">{displayAddress}</div>
+        </div>
+        <div className="extra content">
+          <span className="right floated">
+            <button
+              onClick={() => this.props.createLocation(this.props.crawl, bar)}
+              className="mini ui secondary basic button"
+            >
+              Add Bar
+            </button>
+          </span>
+          <span>Rating: {bar.rating} Stars</span>
+        </div>
       </div>
-      <div className="extra content">
-        <span className="right floated">{bar.city}</span>
-        <span>Rating: {bar.rating} Stars</span>
-      </div>
-    </div>
-  );
+    );
+  }
+}
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     createLocation: (event) => dispatch(createLocation(event))
+//   };
+// };
+
+const mapStateToProps = state => {
+  return {};
 };
 
-export default BarItem;
+export default connect(
+  mapStateToProps,
+  { createLocation }
+)(BarItem);
